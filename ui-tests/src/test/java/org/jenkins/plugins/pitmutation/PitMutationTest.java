3efee2574a9d5f2f9e83f4410e37ebd591a1af4f
Johannes Walter <jwalter@hm.edu>
2022-01-13 13:47:01 +0100

    Testdata useable

--- ui-tests/src/test/java/org/jenkins/plugins/pitmutation/PitMutationTest.java
+++ ui-tests/src/test/java/org/jenkins/plugins/pitmutation/PitMutationTest.java
@@ -1,10 +1,14 @@
 package org.jenkins.plugins.pitmutation;
 
+import org.jenkinsci.test.acceptance.junit.Resource;
 import org.jenkinsci.test.acceptance.junit.WithPlugins;
 import org.jenkinsci.test.acceptance.po.Build;
 import org.jenkinsci.test.acceptance.po.Job;
 import org.jenkinsci.test.acceptance.po.WorkflowJob;
 import org.junit.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PitMutationTest extends UiTest {
 
     //@Test
-    public void BuildSuccessful() {
+    public void BuildSuccessful() throws MalformedURLException {
         Build build = createAndBuildWorkflowJob();
         ConsoleView consoleView = new ConsoleView(build, "console");
 
@@ -21,7 +25,7 @@ public class PitMutationTest extends UiTest {
     }
 
     @Test
-    public void PitMutationReport() {
+    public void PitMutationReport() throws MalformedURLException {
         Build build = createAndBuildWorkflowJob();
 
         DashboardView dashboardView = new DashboardView(build, "pitmutation");
@@ -30,16 +34,33 @@ public class PitMutationTest extends UiTest {
         System.out.println("asd");
     }
 
-    private Build createAndBuildWorkflowJob() {
+    private Build createAndBuildWorkflowJob() throws MalformedURLException {
         return buildJob(createWorkflowJob());
     }
 
-    private WorkflowJob createWorkflowJob() {
+    private WorkflowJob createWorkflowJob() throws MalformedURLException {
         WorkflowJob job = jenkins.jobs.create(WorkflowJob.class);
+
+        //String a = job.copyResourceStep("/PitMutationTest/edu.hm.hafner.util/PathUtil.java.html");
+        //a = a.replace("mkdir -p PathUtil.java.html", "mkdir -p edu.hm.hafner.util/PathUtil.java.html");
+        //a = a.replace("rm -r PathUtil.java.html", "rm -r edu.hm.hafner.util/PathUtil.java.html");
+       // a = a.replace("&& base64", "&& mkdir edu.hm.hafner.util && base64");
+       // a = a.replace("> PathUtil.java.html ", "> edu.hm.hafner.util/PathUtil.java.html");
+        String zipFile = job.copyResourceStep("/PitMutationTest/testdata.zip");
+        zipFile += "\n sh '''unzip testdata.zip'''";
+
         job.script.set("node {\n" +
+            zipFile + "\n" +
             job.copyResourceStep("/PitMutationTest/mutations.xml") + "\n" +
+            //a + "\n" +
+            job.copyResourceStep("/PitMutationTest/edu.hm.hafner.util/PathUtil.java.html") + "\n" +
             "pitmutation killRatioMustImprove: false, minimumKillRatio: 50.0, mutationStatsFile: '**.xml'\n" +
             "}");
+
+        /*
+        * ZIP datei probieren und unpacken
+        * Plugin: Resource Plugin
+        * */
 
         job.save();
         return job;
